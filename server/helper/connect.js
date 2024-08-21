bf2744b39d7c34254cee2c30f6fedf36e6802401
rishPR <rishurawat1703@gmail.com>
2024-08-21 23:12:19 +0530

    changes

--- server/helper/connect.js
+++ server/helper/connect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const connect = async()=>{
+export const connect = async()=>{
    try{
      await mongoose.connect("mongodb+srv://rishurawat1703:<db_password>@cluster0.zuirt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {useNewUrlParser: true, useUnifiedTopology: true});
      console.log("Connected to MongoDB");
