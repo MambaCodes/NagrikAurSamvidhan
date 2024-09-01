0e77277c8f3ef58659353393f753ab2e4580e315
sej-allt <sejalltt@gmail.com>
2024-09-01 13:33:08 +0530

    added games-3

---
+++ games/hangman-game/scripts/data.js
@@ -0,0 +1,18 @@
+// var config = [
+
+//     /****************************************** 		level 1 ----------- animals 		******************************************/
+//     {level:"1", theme: "animals", question: "When and which committee suggested the justiciable and non justiciable types of Rights?", answers: ["Sapru Committee", "Balwant Rai Mehta Committee", "Bhurelal committee", "None of the above"], correct: 1},
+//     {level:"1", theme: "animals", question: "What are the ideals that a state must keep in mind while framing policies called?", answers: ["Fundamental Rights ", "Directive Principles of State Policy", "Gandhian Principles", "Socialism"], correct: 2},
+//     {level:"1", theme: "animals", question: "From which did Minification concept originate?", answers: ["JavaScript code efficiency", "JavaScript interpreter", "Both a and b", "None of the mentioned"], correct: 2},
+//     {level:"1", theme: "animals", question: "Which of the following statements is true regarding DPSP?    i) The concept was mentioned in Government of India Act 1935   ii) DPSP are not legally enforceable", answers: ["Only i", "Only ii", "Both i and ii", "None of the above "], correct: 3},
+//     {level:"1", theme: "animals", question: "Which of the following articles consists of equal pay for equal work for men and women?", answers: ["Article 31", "Article 39A", "Article 41", "Article 39"], correct: 3},
+//     {level:"1", theme: "animals", question: "Which article states about Uniform Civil Code throughout the country?", answers: ["Article 48", "Article 44", "Article 42", "Article 49"], correct: 1},
+//     {level:"1", theme: "animals", question: "Which of the new DPSP was added to the Constitution through the 42nd amendment?", answers: ["To promote equal justice and to provide free legal aid to the poor", "Promote international peace and security between nations", "State need to minimise inequalities in income, status, facilities and opportunities", "Prohibit the consumption of intoxicating drinks and drugs "], correct: 1},
+// ]    ;
+
+export const answers =[
+    { statement : "When and which committee suggested the justiciable and non justiciable types of Rights?", word : "Sapru Committee"},
+    { statement : "What are the ideals that a state must keep in mind while framing policies called?", word : "Directive Principles"},
+    { statement : "Which article  consists of equal pay for equal work for men and women?", word : "Article 41"},
+    { statement : "Which article states about Uniform Civil Code throughout the country?", word : "Article 48"},
+]
