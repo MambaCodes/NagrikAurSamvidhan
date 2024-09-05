7b2b9029fbc264d754169dfeae1d7d801698253e
iamDyeus <dyeusyt@gmail.com>
2024-09-05 17:34:07 +0530

    feat: Add new navbar for Landing Pages

---
+++ client/src/components/Nav/LandingNav2.jsx
@@ -0,0 +1,166 @@
+
+import { Button } from "@/components/ui/button";
+import {
+NavigationMenu,
+NavigationMenuContent,
+NavigationMenuItem,
+NavigationMenuLink,
+NavigationMenuList,
+NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import { Menu, MoveRight, ScaleIcon, X } from "lucide-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+
+export const LandingNav2 = () => {
+const navigationItems = [
+    {
+    title: "Home",
+    href: "/",
+    description: "",
+    },
+    {
+    title: "How it works",
+    href: "/how",
+    description: "",
+    },
+    {
+    title: "About us",
+    href: "/about",
+    description: "",
+    },
+    {
+    title: "Games",
+    description: "Managing a small business today is already tough.",
+    items: [
+        {
+        title: "Reports",
+        href: "/reports",
+        },
+        {
+        title: "Statistics",
+        href: "/statistics",
+        },
+        {
+        title: "Dashboards",
+        href: "/dashboards",
+        },
+        {
+        title: "Recordings",
+        href: "/recordings",
+        },
+    ],
+    }
+];
+
+const [isOpen, setOpen] = useState(false);
+const navigate = useNavigate();
+return (
+    <header className="w-full z-40 fixed top-0 left-0 bg-background">
+    <div className="container relative mx-auto min-h-20 flex gap-4 flex-row lg:grid lg:grid-cols-3 items-center">
+        <div className="justify-start items-center gap-4 lg:flex hidden flex-row">
+        <NavigationMenu className="flex justify-start items-start">
+            <NavigationMenuList className="flex justify-start gap-4 flex-row">
+            {navigationItems.map((item) => (
+                <NavigationMenuItem key={item.title}>
+                {item.href ? (
+                    <>
+                    <NavigationMenuLink>
+                        {/*  make it use navigate on click */}
+                        <Button variant="ghost" onClick={() => navigate(item.href)} >{item.title}</Button>
+                    </NavigationMenuLink>
+                    </>
+                ) : (
+                    <>
+                    <NavigationMenuTrigger className="font-medium text-sm">
+                        {item.title}
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent className="!w-[450px] p-4">
+                        <div className="flex flex-col lg:grid grid-cols-2 gap-4">
+                        <div className="flex flex-col h-full justify-between">
+                            <div className="flex flex-col">
+                            <p className="text-base">{item.title}</p>
+                            <p className="text-muted-foreground text-sm">
+                                {item.description}
+                            </p>
+                            </div>
+                            <Button size="sm" className="mt-10">
+                            Book a call today
+                            </Button>
+                        </div>
+                        <div className="flex flex-col text-sm h-full justify-end">
+                            {item.items?.map((subItem) => (
+                            <NavigationMenuLink
+                                href={subItem.href}
+                                key={subItem.title}
+                                className="flex flex-row justify-between items-center hover:bg-muted py-2 px-4 rounded"
+                            >
+                                <span>{subItem.title}</span>
+                                <MoveRight className="w-4 h-4 text-muted-foreground" />
+                            </NavigationMenuLink>
+                            ))}
+                        </div>
+                        </div>
+                    </NavigationMenuContent>
+                    </>
+                )}
+                </NavigationMenuItem>
+            ))}
+            </NavigationMenuList>
+        </NavigationMenu>
+        </div>
+        <div className="flex lg:justify-center" onClick={()=>navigate("/")}>
+        <ScaleIcon className="cursor-pointer" />
+        </div>
+        <div className="flex justify-end w-full gap-4">
+        <Button variant="ghost" className="hidden md:inline" onClick={()=>navigate("faqs")}>
+            FAQ
+        </Button>
+        <div className="border-r hidden md:inline"></div>
+        <Button variant="outline">Sign in</Button>
+        <Button>Get started</Button>
+        </div>
+        <div className="flex w-12 shrink lg:hidden items-end justify-end">
+        <Button variant="ghost" onClick={() => setOpen(!isOpen)}>
+            {isOpen ? <X className="w-5 h-5" /> : <Menu className="w-5 h-5" />}
+        </Button>
+        {isOpen && (
+            <div className="absolute top-20 border-t flex flex-col w-full right-0 bg-background shadow-lg py-4 container gap-8">
+            {navigationItems.map((item) => (
+                <div key={item.title}>
+                <div className="flex flex-col gap-2">
+                    {item.href ? (
+                    <Link
+                        href={item.href}
+                        className="flex justify-between items-center"
+                    >
+                        <span className="text-lg">{item.title}</span>
+                        <MoveRight className="w-4 h-4 stroke-1 text-muted-foreground" />
+                    </Link>
+                    ) : (
+                    <p className="text-lg">{item.title}</p>
+                    )}
+                    {item.items &&
+                    item.items.map((subItem) => (
+                        <Link
+                        key={subItem.title}
+                        href={subItem.href}
+                        className="flex justify-between items-center"
+                        >
+                        <span className="text-muted-foreground">
+                            {subItem.title}
+                        </span>
+                        <MoveRight className="w-4 h-4 stroke-1" />
+                        </Link>
+                    ))}
+                </div>
+                </div>
+            ))}
+            </div>
+        )}
+        </div>
+    </div>
+    </header>
+);
+};
--- client/src/pages/Landing/LandingLayout.jsx
+++ client/src/pages/Landing/LandingLayout.jsx
@@ -1,11 +1,12 @@
 import LandingNav from '@/components/Nav/LandingNav'
+import { LandingNav2 } from '@/components/Nav/LandingNav2'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 function LandingLayout() {
   return (
-    <>
-    <LandingNav/>
+    <> 
+    <LandingNav2/>
     <Outlet/>
     </>
   )
